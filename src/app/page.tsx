597f336edca4ecf874df8f343bf0f2344d5023a6
themichellesarmiento <michellechicsarmiento@yahoo.com>
2026-08-20 09:25:47 +0200

    selecting answers function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,12 @@ export default function Home() {
   const [questionNumber, setQuestionNumber] = useState(0)
   const [selectedAnswers, setSelectedAnswers] = useState<string[]>([]);
 
+
+  const handleSelectedAnswers = (type: string) => {
+    setSelectedAnswers(curr => [...curr, type])
+    setQuestionNumber(curr => curr + 1) //move to the next question
+  }
+
   const handlePrev = () => {
     setQuestionNumber(curr => Math.max(curr - 1, 0))
     setSelectedAnswers(curr => curr.slice(0, -1)) //drop last respomse
@@ -16,7 +22,7 @@ export default function Home() {
   return (
     <main className='flex flex-col'>
       <ProgressBar currentNumber={questionNumber} />
-      <QA currentNumber={questionNumber} selectAnswer={() => { }} onPrev={handlePrev} />
+      <QA currentNumber={questionNumber} selectAnswer={handleSelectedAnswers} onPrev={handlePrev} />
     </main>
   );
 }
